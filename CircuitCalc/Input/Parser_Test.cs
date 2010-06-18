529921ecaad04763622e8370d0c21abf721c465b
xoposhiy@mail.ru <xoposhiy@mail.ru@f78b6cbc-4a89-5bc9-8d0a-bb4248158ca5>
2010-06-18 19:46:05 +0000

    Парсер схем заводов.

---
+++ CircuitCalc/Input/Parser_Test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CircuitCalc.Input
+{
+	[TestFixture]
+	public class Parser_Test
+	{
+		[Test]
+		public void TestCase()
+		{
+			var parser = new Parser();
+			parser.Parse("sample.txt", new ConsoleSink());
+		}
+	}
+
+	public class ConsoleSink : ISink
+	{
+		public void OnGate(int gateIndex, Point leftIn, Point rightIn, Point leftOut, Point rightOut)
+		{
+			Console.WriteLine("{0}: {1}, {2} -> {3}, {4}", gateIndex, leftIn, rightIn, leftOut, rightOut);
+		}
+
+		public void OnInput(Point to)
+		{
+			Console.WriteLine(to);
+		}
+
+		public void OnOutput(Point from)
+		{
+			Console.WriteLine(from);
+		}
+	}
+}
