2e0f774992b80a342892528f353a3cbb88430c4c
xoposhiy@mail.ru <xoposhiy@mail.ru@f78b6cbc-4a89-5bc9-8d0a-bb4248158ca5>
2010-06-18 21:24:06 +0000

    Выполнятор схем бензофабрик

---
+++ trunk/CircuitCalc/PeCalc/Copy of Gate.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using CircuitCalc.Input;
+
+namespace CircuitCalc.PeCalc
+{
+	class Gate
+	{
+		public Gate()
+		{
+			lout = () => L(lin(), rin());
+			rout = () => R(lin(), rin());
+		}
+
+		public Func<int> lin, rin;
+		public readonly Func<int> lout, rout;
+
+		public int R(int leftIn, int rightIn)
+		{
+			return (2 + (leftIn * leftIn)) % 3;
+		}
+
+		public int L(int leftIn, int rightIn)
+		{
+			return (leftIn * (1 + rightIn + rightIn * rightIn) + 2 * rightIn) % 3;
+		}
+
+		public void SetIn(char side, Func<int> get)
+		{
+			if(side == 'R') rin = get;
+			else if(side == 'L') lin = get;
+			else throw new Exception("Unknown side " + side);
+		}
+
+		public Func<int> GetOut(char side)
+		{
+			if(side == 'R') return rout;
+			if(side == 'L') return lout;
+			throw new Exception("Unknown side " + side);
+		}
+	}
+
+	class Calculator : ISink
+	{
+		private readonly string input;
+		public Dictionary<int, Gate> gates = new Dictionary<int, Gate>();
+		private Func<int> outputChar;
+		private int position;
+
+		public Calculator(string filename, string input)
+		{
+			this.input = input;
+			new Parser().Parse(filename, this);
+		}
+
+		public int Next()
+		{
+			return outputChar();
+		}
+
+		public void OnGate(int gateIndex, Point leftIn, Point rightIn, Point leftOut, Point rightOut)
+		{
+			Func<int> theOut = GetTheOut(leftIn, gateIndex);
+			GetGate(gateIndex).SetIn('L', theOut);
+
+			theOut = GetTheOut(rightIn, gateIndex);
+			GetGate(gateIndex).SetIn('R', theOut);
+		}
+
+		private Func<int> GetTheOut(Point leftIn, int gateIndex)
+		{
+			if(leftIn.gate < 0) return ReadNext;
+			Func<int> theOut = GetGate(leftIn.gate).GetOut(leftIn.side);
+			if(leftIn.gate <= gateIndex) theOut = new BackWire(theOut).Get;
+			return theOut;
+		}
+
+		public void OnInput(Point to)
+		{
+			GetGate(to.gate).SetIn(to.side, ReadNext);
+		}
+
+		private int ReadNext()
+		{
+			if (position >= input.Length) return 0;
+			return input[position++] - '0';
+		}
+
+		private Gate GetGate(int index)
+		{
+			Gate gate;
+			if (!gates.TryGetValue(index, out gate))
+			{
+				gate = new Gate();
+				gates[index] = gate;
+			}
+			return gate;
+		}
+
+		public void OnOutput(Point from)
+		{
+			outputChar = GetGate(from.gate).GetOut(from.side);
+		}
+	}
+
+	public class BackWire
+	{
+		private int mem = 0;
+		public Func<int> get;
+
+		public BackWire(Func<int> get)
+		{
+			this.get = get;
+		}
+
+		public int Get()
+		{
+			int result = mem;
+			mem = get();
+			return result;
+		}
+	}
+}
---
+++ trunk/CircuitCalc/PeCalc/Gate.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using CircuitCalc.Input;
+
+namespace CircuitCalc.PeCalc
+{
+	class Gate
+	{
+		public Gate()
+		{
+			lin = c =>
+			      	{
+			      		fromL = c;
+			      		TryPush();
+			      	};
+			rin = c =>
+			       	{
+			       		fromR = c;
+			       		TryPush();
+			       	};
+		}
+
+		private void TryPush()
+		{
+			if(fromR >= 0 && fromL >= 0)
+			{
+				lout(L(fromL, fromR));
+				rout(R(fromL, fromR));
+			}
+		}
+
+		public void NextStep()
+		{
+			fromR = -1;
+			fromL = -1;
+		}
+
+		public int fromR, fromL;
+		public readonly Action<int> lin, rin;
+		public Action<int> lout, rout;
+
+		public int R(int leftIn, int rightIn)
+		{
+			int res = (2 + (leftIn * rightIn)) % 3;
+			//Console.WriteLine("R({0}, {1}) = {2}", leftIn, rightIn, res);
+			return res;
+		}
+
+		public int L(int leftIn, int rightIn)
+		{
+			int res = (leftIn * (1 + rightIn + rightIn * rightIn) + 2 * rightIn) % 3;
+			//Console.WriteLine("L({0}, {1}) = {2}", leftIn, rightIn, res);
+			return res;
+		}
+
+		public void SetOut(char side, Action<int> push)
+		{
+			if(side == 'R') rout = push;
+			else if(side == 'L') lout = push;
+			else throw new Exception("Unknown side " + side);
+		}
+
+		public Action<int> GetIn(char side)
+		{
+			if(side == 'R') return rin;
+			if(side == 'L') return lin;
+			throw new Exception("Unknown side " + side);
+		}
+	}
+
+	class Calculator : ISink
+	{
+		private readonly string input;
+		public Dictionary<int, Gate> gates = new Dictionary<int, Gate>();
+		public IList<BackWire> backwires = new List<BackWire>();
+		private int position;
+		private Action<int> pushChar;
+
+		public Calculator(string filename, string input)
+		{
+			this.input = input;
+			new Parser().Parse(filename, this);
+		}
+
+		public void PushNext()
+		{
+			foreach(var gate in gates.Values)
+				gate.NextStep();
+			foreach(var backWire in backwires)
+				backWire.NextStep();
+			int ch = (position >= input.Length ? '0' : input[position++]) - '0';
+			//Console.WriteLine("input gives " + ch);
+			pushChar(ch);
+		}
+
+		public void OnGate(int gateIndex, Point leftIn, Point rightIn, Point leftOut, Point rightOut)
+		{
+			Action<int> theIn = GetTheIn(leftOut, gateIndex);
+			GetGate(gateIndex).SetOut('L', theIn);
+
+			theIn = GetTheIn(rightIn, gateIndex);
+			GetGate(gateIndex).SetOut('R', theIn);
+		}
+
+		private Action<int> GetTheIn(Point point, int gateIndex)
+		{
+			if(point.gate < 0) return OutputChar;
+			Action<int> theIn = GetGate(point.gate).GetIn(point.side);
+			if(point.gate <= gateIndex)
+			{
+				var backWire = new BackWire(theIn);
+				backwires.Add(backWire);
+				theIn = backWire.Push;
+			}
+			return theIn;
+		}
+
+		public void OnInput(Point to)
+		{
+			pushChar = GetGate(to.gate).GetIn(to.side);
+		}
+
+		private Gate GetGate(int index)
+		{
+			Gate gate;
+			if (!gates.TryGetValue(index, out gate))
+			{
+				gate = new Gate();
+				gates[index] = gate;
+			}
+			return gate;
+		}
+
+		public void OnOutput(Point from)
+		{
+			GetGate(from.gate).SetOut(from.side, OutputChar);
+		}
+
+		private static void OutputChar(int ch)
+		{
+			Console.Write(ch);
+		}
+	}
+
+	public class BackWire
+	{
+		private int mem;
+		public Action<int> push;
+
+		public BackWire(Action<int> push)
+		{
+			this.push = push;
+		}
+
+		public void Push(int ch)
+		{
+			mem = ch;
+		}
+
+		public void NextStep()
+		{
+			push(mem);
+			//Console.WriteLine("backwire gives " + mem);
+		}
+	}
+}
