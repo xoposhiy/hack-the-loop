8d63609acd568bbeeb41693f68f6b46d8f257902
xoposhiy@mail.ru <xoposhiy@mail.ru@f78b6cbc-4a89-5bc9-8d0a-bb4248158ca5>
2010-06-18 21:24:06 +0000

    Выполнятор схем бензофабрик

---
+++ CircuitCalc/PeCalc/Calc_Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CircuitCalc.PeCalc
+{
+	[TestFixture]
+	public class Calc_Test
+	{
+		[Test]
+		public void TestCase()
+		{
+			var calculator = new Calculator("sample.txt", "0222220210110011");
+			for(int i = 0; i < 17; i++) calculator.PushNext();
+		}
+	}
+}
