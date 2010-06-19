83ae34b3d0b37453319e50199f8dcb2dc5deb145
Andrew.Kostousov <Andrew.Kostousov@f78b6cbc-4a89-5bc9-8d0a-bb4248158ca5>
2010-06-19 13:28:38 +0000

    Ерунда всякая.

--- trunk/CircuitCalc/Program.cs
+++ trunk/CircuitCalc/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace CircuitCalc
@@ -7,7 +9,36 @@ namespace CircuitCalc
 	{
 		static void Main(string[] args)
 		{
-			
+			CodeMonkeysGen(args);
+		}
+
+		private static void CodeMonkeysGen(string[] args)
+		{
+			int size = 10;
+			if (args.Length > 0)
+			{
+				size = int.Parse(args[0]);
+			}
+			string fOut = "output.txt";
+			if (args.Length > 1)
+			{
+				fOut = args[1];
+			}
+			var pw = new StringBuilder();
+			pw.AppendFormat("{0}L:", size);
+			pw.AppendLine();
+			pw.AppendFormat("{0}R{1}L0#{2}R{3}R,", 1, 0, 0, size);
+			pw.AppendLine();
+			for (int i = 1; i < size; i++)
+			{
+				pw.AppendFormat("{0}R{1}L0#{2}R{3}L,", i + 1, i, i, i - 1);
+				pw.AppendLine();
+			}
+			pw.AppendFormat("X{0}R0#X{1}L:", 0, size - 1);
+			pw.AppendLine();
+			pw.AppendFormat("{0}L", size);
+			pw.AppendLine();
+			File.WriteAllText(fOut, pw.ToString());
 		}
 	}
 }
---
+++ trunk/CircuitCalc/Trit.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+using System.Linq;
+
+namespace CircuitCalc
+{
+	class Trit
+	{
+		public static int ToDec(string s)
+		{
+			int res = 0;
+			int pow = 1;
+			for (var i = s.Length - 1; i >= 0; i--)
+			{
+				res += pow*byte.Parse(s[i].ToString());
+				pow *= 3;
+			}
+			return res;
+		}
+
+		public static string ToTrits(int n)
+		{
+			var sb = new StringBuilder();
+			while (n > 0)
+			{
+				sb.Append(n%3);
+				n /= 3;
+			}
+			return new string(sb.ToString().ToCharArray().Reverse().ToArray());
+		}
+	}
+
+	[TestFixture]
+	public class Trit_Test
+	{
+		[Test]
+		public void Calc()
+		{
+			Console.WriteLine(Trit.ToDec("122000010"));
+			Console.WriteLine(Trit.ToDec("12200001"));
+			Console.WriteLine(Trit.ToDec("1220000"));
+			Console.WriteLine(Trit.ToDec("122000"));
+			Console.WriteLine(Trit.ToDec("12200"));
+			Console.WriteLine(Trit.ToDec("1220"));
+			Console.WriteLine(Trit.ToDec("122"));
+			Console.WriteLine(Trit.ToDec("12"));
+			Console.WriteLine(Trit.ToDec("122"));
+			Console.WriteLine(Trit.ToDec("220"));
+			Console.WriteLine(Trit.ToDec("221"));
+			Console.WriteLine(Trit.ToDec("222"));
+			Console.WriteLine(Trit.ToDec("10022111"));
+		}
+
+		[Test]
+		public void ToTrits()
+		{
+			Console.WriteLine(Trit.ToTrits(2416));
+			Console.WriteLine(Trit.ToTrits(2711));
+		}
+	}
+}
