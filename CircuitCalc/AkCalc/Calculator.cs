9472092695bc419cdbadcfca0353043bdc029317
Andrew.Kostousov <Andrew.Kostousov@f78b6cbc-4a89-5bc9-8d0a-bb4248158ca5>
2010-06-19 06:01:32 +0000

    Недописанный "тянущий" калькулятор.

--- CircuitCalc/AkCalc/Calculator.cs
+++ CircuitCalc/AkCalc/Calculator.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Enumerable = System.Linq.Enumerable;
 
-namespace CircuitCalc
+namespace CircuitCalc.AkCalc
 {
 	class Calculator
 	{
@@ -16,11 +15,28 @@ namespace CircuitCalc
 
 		private static void Calc(Circuit circuit, IEnumerable<byte> input)
 		{
+			foreach (var b in input)
+			{
+				circuit.X.Output = b;
+				var res = circuit.X.Link.GetResult();
+				Console.Write(res);
+				NextStep(circuit);
+			}
+		}
+
+		private static void NextStep(Circuit circuit)
+		{
+			foreach(var node in circuit.Nodes)
+			{
+				node.Visited = false;
+				node.LeftOutput = node.NewLeftOutput;
+				node.RightOutput = node.NewRightOutput;
+			}
 		}
 
 		private static IEnumerable<byte> ReadInput(string input)
 		{
-			return Enumerable.ToList<byte>(input.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(c => byte.Parse(c)));
+			return input.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(c => byte.Parse(c)).ToList();
 		}
 
 		private static Circuit ReadCircuit(string circuit)
@@ -34,4 +50,6 @@ namespace CircuitCalc
 			return null;
 		}
 	}
+
+	
 }
