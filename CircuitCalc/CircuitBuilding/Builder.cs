755232068231e6cdae5219053c271b864da65bdc
xoposhiy@mail.ru <xoposhiy@mail.ru@f78b6cbc-4a89-5bc9-8d0a-bb4248158ca5>
2010-06-19 14:29:15 +0000

    Интегрируемся...

--- CircuitCalc/CircuitBuilding/Builder.cs
+++ CircuitCalc/CircuitBuilding/Builder.cs
@@ -1,26 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using CircuitCalc.PeCalc;
 
 namespace CircuitCalc.CircuitBuilding
 {
 	class Builder
 	{
-		private readonly string input;
-		private readonly string wantedOutput;
+		private readonly byte[,] o_kt;
+		private readonly byte[] o_t0;
+		private readonly int n;
 
 		public Builder(string input, string wantedOutput)
 		{
-			this.input = input;
-			this.wantedOutput = wantedOutput;
+			n = wantedOutput.Length;
+			o_t0 = new byte[n];
+			o_kt = new byte[n,n];
+			for(int t=0; t<n; t++)
+				o_kt[0, t] = GetRightInput(Char2Byte(input[t]), Char2Byte(wantedOutput[t]));
+			o_t0[0] = o_kt[0, 0];
 		}
-		
+
+		private byte Char2Byte(char ch)
+		{
+			return (byte) (ch - '0');
+		}
+
+		// http://clip2net.com/clip/m21010/1276953465-clip-2kb.png
+		private byte GetRightInput(byte leftIn, byte leftOut)
+		{
+			return (byte) ((leftIn - leftOut + 3)%3); //rin(lin, lout) = Lout(lin, lout)! wow!
+		}
+
 		/// <returns>i-ый элемент - это out(i, 0) - 0, 1 или 2</returns>
 		public byte[] Build()
 		{
-			//TODO
-			return new byte[0];
+			for (int k=1; k<n; k++)
+			{
+				for (int t = 0; t < n-k; t++)
+					o_kt[k, t] = GetWantedInput(k-1, t + 1);
+				o_t0[k] = o_kt[k, 0];
+			}
+			return o_t0;
+		}
+
+		private byte GetWantedInput(int k, int t)
+		{
+			byte hType = o_kt[k, 0];
+			var wantedOut = o_kt[k, t];
+			string prefix = BuildPrefix(k, t - 1);
+			var wantedChar = (char) (wantedOut + '0');
+			var schemeName = string.Format("h{0}.txt", hType);
+			if (LastCharFor(schemeName, prefix + '0') == wantedChar) return 0;
+			if (LastCharFor(schemeName, prefix + '1') == wantedChar) return 1;
+			if (LastCharFor(schemeName, prefix + '2') == wantedChar) return 2;
+			throw new Exception("все плохо. h1, prefix:" + prefix + ", wantedChar:" + wantedChar);
+		}
+
+		private char LastCharFor(string filename, string inp)
+		{
+			return new Calculator(filename).PushString(inp).Last();
+		}
+
+		private string BuildPrefix(int k, int lastT)
+		{
+			string s = "0";
+			for(int t=1; t<=lastT; t++)
+			{
+				var in_kt = o_kt[k + 1, t-1];
+				s += (char)('0' + in_kt);
+			}
+			return s;
 		}
 	}
 }
---
+++ CircuitCalc/CircuitBuilding/Builder_Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CircuitCalc.PeCalc;
+using NUnit.Framework;
+
+namespace CircuitCalc.CircuitBuilding
+{
+	[TestFixture]
+	public class Builder_Test
+	{
+		[Test]
+		public void H1()
+		{
+			var bytes = new Builder("01202101210201202", "11021210112101221").Build();
+			foreach (var b in bytes)
+			{
+				Console.Write(b + " ");
+			}
+			var serialize = new CircuitSerializer().Serialize(bytes);
+			Console.WriteLine(serialize);
+		}
+
+		[Test]
+		public void Requirement_description()
+		{
+			Console.WriteLine(new Calculator("h2.txt").PushString("01202101210201202"));
+		}
+
+	}
+}
