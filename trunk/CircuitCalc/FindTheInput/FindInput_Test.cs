ef137c7c2c18ef60a82945475c811f079e5c4861
xoposhiy@mail.ru <xoposhiy@mail.ru@f78b6cbc-4a89-5bc9-8d0a-bb4248158ca5>
2010-06-19 09:24:59 +0000

    фиксим баги

--- trunk/CircuitCalc/FindTheInput/FindInput_Test.cs
+++ trunk/CircuitCalc/FindTheInput/FindInput_Test.cs
@@ -1,47 +1,78 @@
 ﻿using System;
 using CircuitCalc.PeCalc;
 using NUnit.Framework;
+using System.Linq;
 
 namespace CircuitCalc.FindTheInput
 {
+	public class Fact{
+		public Fact(string filename, string knownOutput)
+		{
+			this.filename = filename;
+			this.knownOutput = knownOutput;
+		}
+		public bool IsGoodInput(string input)
+		{
+			Console.WriteLine(filename);
+			string realOut = new Calculator(filename).PushString(input);
+			bool isGoodInput = knownOutput.StartsWith(realOut);
+			if (!isGoodInput) Console.WriteLine("{0} -> {1}, expected: {2}   ({3})", input, realOut, knownOutput, filename);
+			return isGoodInput;
+		}
+
+		private string filename;
+		private string knownOutput;
+	}
 	[TestFixture]
 	public class FindInput_Test
 	{
 		[Test]
+		public void Simple2()
+		{
+			string input = "012021";
+			string res = new Calculator("simple2.txt").PushString(input);
+			Console.WriteLine("{0} -> {1}", input, res);
+		}
+
+		[Test]
 		public void TestCase()
 		{
-			Func<Calculator> createSimple = () => new Calculator("simple.txt");
-			Func<Calculator> createSimple2 = () => new Calculator("simple2.txt");
-			var simpleResult = "02120112100002120";
-			var simple2Result = "01210221200001210";
 			string prefix = "";
-			for(int i = 0; i< simpleResult.Length; i++)
+			for(int i = 0; i< 17; i++)
 			{
-				prefix = TryImprovePrefix(simple2Result, prefix, createSimple2);
-				if(prefix.Length == i)
-					prefix = TryImprovePrefix(simpleResult, prefix, createSimple);
-				if(prefix.Length == i)
+				bool r0 = Try(prefix+'0');
+				bool r1 = Try(prefix+'1');
+				bool r2 = Try(prefix+'2');
+				int c = 0;
+				if(r0) c++;
+				if(r1) c++;
+				if(r2) c++;
+				if (c != 1)
 				{
-					Console.WriteLine("FAIL");
+					Console.WriteLine("FAIL! prefix: {0}({1}{2}{3})", prefix, r0?"0":"", r1?"1":"", r2?"2":"");
 					break;
+				}
+				else
+				{
+					if(r0) prefix += '0';
+					if(r1) prefix += '1';
+					if(r2) prefix += '2';
+					Console.WriteLine("new prefix: {0}", prefix);
 				}
 			}
 		}
-
-		private string TryImprovePrefix(string actualResult, string prefix, Func<Calculator> createScheme)
-		{
-			for(char next = '0'; next < '3'; next++)
+		private readonly Fact[] facts = new[]
 			{
-				string candidate = prefix+next;
-				string result = createScheme().PushString(candidate);
-				if (actualResult.StartsWith(result))
-				{
-					prefix = candidate;
-					Console.WriteLine("PREFIX IS: {0} on OUTPUT {1}", prefix, result);
-					break;
-				}
-			}
-			return prefix;
+				new Fact("simple.txt",  "02120112100002120"), 
+				new Fact("simple2.txt", "01210221200001210"), 
+//				new Fact("simple3.txt", "22022022022022022"), 
+				//new Fact("sample.txt", "10221220002011011"), 
+			};
+
+		private bool Try(string input)
+		{
+			Console.WriteLine("TRY: {0}", input);
+			return facts.All(f => f.IsGoodInput(input));
 		}
 	}
 }
--- trunk/CircuitCalc/PeCalc/Calc_Test.cs
+++ trunk/CircuitCalc/PeCalc/Calc_Test.cs
@@ -6,10 +6,12 @@ namespace CircuitCalc.PeCalc
 	[TestFixture]
 	public class Calc_Test
 	{
+		private const string twiceRealOut = "12211010022120012";
+
 		[Test]
 		public void TestCase()
 		{
-			var calculator = new Calculator("sample.txt");
+			var calculator = new Calculator("twice.txt");
 			const string sampleIn = "02222220210110011";
 			const string realIn = "02102202202202202";
 			const string sampleRealOut = "10221220002011011";
