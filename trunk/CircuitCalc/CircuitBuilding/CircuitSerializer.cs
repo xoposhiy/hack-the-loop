1b638bf5d81838f5ad760ae9d088087ce1781d0a
Andrew.Kostousov <Andrew.Kostousov@f78b6cbc-4a89-5bc9-8d0a-bb4248158ca5>
2010-06-19 15:16:01 +0000

    Сериализация фабрики - багфиксы

--- trunk/CircuitCalc/CircuitBuilding/CircuitSerializer.cs
+++ trunk/CircuitCalc/CircuitBuilding/CircuitSerializer.cs
@@ -17,46 +17,49 @@ namespace CircuitCalc.CircuitBuilding
 			var idxOut = 0;
 			for (var k = 0; k < primitives.Length; k++)
 			{
-				idxIn = k == primitives.Length ? lastIdx : primitiveSizes.Take(k + 2).Sum() - 1;
+				var lastPrimitive = k == primitives.Length - 1;
+				char inSide = lastPrimitive ? 'R' : 'L';
+				idxIn = lastPrimitive ? lastIdx : primitiveSizes.Take(k + 2).Sum() - 1;
 				var firstPrimitive = k == 0;
 				char outSide = firstPrimitive ? 'R' : 'L';
 				idxOut = firstPrimitive ? lastIdx : primitiveSizes.Take(k).Sum() - 1;
+				var baseIdx = primitiveSizes.Take(k).Sum();
 				switch (primitives[k])
 				{
 					case 0:
-						SerializeH0(sb, k, idxIn, idxOut, outSide);
+						SerializeH0(sb, baseIdx, idxIn, idxOut, outSide, inSide);
 						break;
 					case 1:
-						SerializeH1(sb, k, idxIn, idxOut, outSide);
+						SerializeH1(sb, baseIdx, idxIn, idxOut, outSide, inSide);
 						break;
 					case 2:
-						SerializeH2(sb, k, idxIn, idxOut, outSide);
+						SerializeH2(sb, baseIdx, idxIn, idxOut, outSide, inSide);
 						break;
 				}
 			}
 			var firstPrimitiveOutIdx = GetPrimitiveSize(primitives.First()) - 1;
-			var lastPrimitiveInIdx = (size - 1) - GetPrimitiveSize(primitives.Last());
+			var lastPrimitiveInIdx = lastIdx - 1;
 			sb.AppendLine(string.Format("X{0}L0#X{1}L:", firstPrimitiveOutIdx, lastPrimitiveInIdx));
 			sb.AppendLine(string.Format("{0}L", lastIdx));
 			return sb.ToString();
 		}
 
-		private static void SerializeH0(StringBuilder sb, int k, int idxIn, int idxOut, char outSide)
+		private static void SerializeH0(StringBuilder sb, int k, int idxIn, int idxOut, char outSide, char inSide)
 		{
-			sb.AppendLine(string.Format("{0}L{1}R0#{2}{3}{1}R,", idxIn, k, idxOut, outSide));
+			sb.AppendLine(string.Format("{0}{4}{1}R0#{2}{3}{1}R,", idxIn, k, idxOut, outSide, inSide));
 		}
 
-		private static void SerializeH1(StringBuilder sb, int k, int idxIn, int idxOut, char outSide)
+		private static void SerializeH1(StringBuilder sb, int k, int idxIn, int idxOut, char outSide, char inSide)
 		{
 			sb.AppendLine(string.Format("{0}L{1}R0#{0}L{1}R,", k, k + 1));
-			sb.AppendLine(string.Format("{0}L{1}R0#{2}{3}{1}R,", idxIn, k, idxOut, outSide));
+			sb.AppendLine(string.Format("{0}{4}{1}R0#{2}{3}{1}R,", idxIn, k, idxOut, outSide, inSide));
 		}
 
-		private static void SerializeH2(StringBuilder sb, int k, int idxIn, int idxOut, char outSide)
+		private static void SerializeH2(StringBuilder sb, int k, int idxIn, int idxOut, char outSide, char inSide)
 		{
 			sb.AppendLine(string.Format("{0}L{1}R0#{0}L{2}R,", k, k + 2, k + 1));
 			sb.AppendLine(string.Format("{0}R{1}R0#{2}R{0}L,", k+1, k, k + 2));
-			sb.AppendLine(string.Format("{0}L{1}L0#{2}{4}{3}R,", idxIn, k + 1, idxOut, k, outSide));
+			sb.AppendLine(string.Format("{0}{5}{1}L0#{2}{4}{3}R,", idxIn, k + 1, idxOut, k, outSide, inSide));
 		}
 
 		private static int GetPrimitiveSize(byte p)
