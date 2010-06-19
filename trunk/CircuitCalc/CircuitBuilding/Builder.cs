e559527ae7a1e2a756238420d4baa3cadec1fba7
xoposhiy@mail.ru <xoposhiy@mail.ru@f78b6cbc-4a89-5bc9-8d0a-bb4248158ca5>
2010-06-19 13:09:47 +0000

    интерфейс интеллектуальной части CircuitBuilder-а

---
+++ trunk/CircuitCalc/CircuitBuilding/Builder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CircuitCalc.CircuitBuilding
+{
+	class Builder
+	{
+		private readonly string input;
+		private readonly string wantedOutput;
+
+		public Builder(string input, string wantedOutput)
+		{
+			this.input = input;
+			this.wantedOutput = wantedOutput;
+		}
+		
+		/// <returns>i-ый элемент - это out(i, 0) - 0, 1 или 2</returns>
+		public byte[] Build()
+		{
+			//TODO
+			return new byte[0];
+		}
+	}
+}
