33fd9d8cbd3e5df49a3815dff3e44add10018cdf
Andrew.Kostousov <Andrew.Kostousov@f78b6cbc-4a89-5bc9-8d0a-bb4248158ca5>
2010-06-20 09:47:16 +0000

    Постилка заводов. Обновлены машинки, они теперь отсортированы по возрастанию идентификаторов
    Авто логин написан, но не работает. Пока на него забью.

---
+++ trunk/WebClient/HtmlParser.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace WebClient
+{
+	static class HtmlParser
+	{
+		public static IEnumerable<string> ParseCarsList(string response)
+		{
+			var matches = CarListItemTemplate.Matches(response);
+			foreach (Match match in matches)
+			{
+				var carId = match.Groups["CarId"].Value;
+				yield return carId;
+			}
+		}
+
+		public static string ParseCar(string response)
+		{
+			var match = CarTemplate.Match(response);
+			var car = match.Groups["Car"].Value;
+			return car;
+		}
+
+		public static string ParseSubmitFuelResponse(string response)
+		{
+			var match = ErrorTemplate.Match(response);
+			var error = match.Success ? match.Groups["Error"].Value : string.Empty;
+			if (string.IsNullOrEmpty(error))
+			{
+				match = SyntaxErrorTemplate.Match(response);
+				error = match.Success ? match.Groups["Error"].Value : string.Empty;
+			}
+			return error;
+		}
+
+		private static readonly Regex CarTemplate = new Regex(@"<label for=.*?>Car:</label>(?<Car>\d*?)</div>");
+		private static readonly Regex CarListItemTemplate = new Regex(@"<tr><td style=.*?>(?<CarId>\d*?)</td><td>(?<NumberOfFuels>\d*?)</td><td>.*?</td></tr>");
+		private static readonly Regex ErrorTemplate = new Regex(@"<span id=""solution.errors"" class=""errors"">(?<Error>.*?)</span>", RegexOptions.Singleline);
+		private static readonly Regex SyntaxErrorTemplate = new Regex(@"<pre>(?<Error>.*?)</pre><form id=""solution""", RegexOptions.Singleline);
+	}
+}
