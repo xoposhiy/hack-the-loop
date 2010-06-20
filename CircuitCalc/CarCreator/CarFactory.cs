ec59091abc240d0a8ddba770752c24e106291db7
last_g@hackerdom.ru <last_g@hackerdom.ru@f78b6cbc-4a89-5bc9-8d0a-bb4248158ca5>
2010-06-20 18:00:04 +0000

    Модификации в машинках, Добавлена библиотека для длинной арифметики

--- CircuitCalc/CarCreator/CarFactory.cs
+++ CircuitCalc/CarCreator/CarFactory.cs
@@ -16,5 +16,23 @@ namespace CircuitCalc.CarCreator
 		{
 			return new Car(null, 1);
 		}
+
+		/// <summary>
+		/// Генератор машинок со сложными ограничениями. В частности:
+		/// Степенное ограничение: одно топливо должно быть в точностью некоей степенью другого топлива
+		/// "Круговое" ограничение то есть граф зависимости, при попытке вычислить какое топливо зависит от которого, является циклическим
+		/// Смотри car1 из CreationTesting
+		/// </summary>
+		/// <param name="pow1">Первое степенное ограничение</param>
+		/// <param name="pow2">Второе степенное ограничение</param>
+		/// <param name="pow3">Третье степенное ограничение</param>
+		public Car GetCarLoopWithLenearStricts(int pow1, int pow2, int pow3)
+		{
+			int baseMatrix = 2;
+			decimal fuel1 = 10000*100000000000;
+			fuel1 *= fuel1;
+			Console.WriteLine(fuel1);
+			return new Car(null, 1);
+		}
 	}
 }
