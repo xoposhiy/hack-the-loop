a290ef32f30da64bf843acba877998d94044bdfc
xoposhiy@mail.ru <xoposhiy@mail.ru@f78b6cbc-4a89-5bc9-8d0a-bb4248158ca5>
2010-06-20 02:39:17 +0000

    T-Parser!

---
+++ trunk/CircuitCalc/TParsing/TParser_Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CircuitCalc.TParsing
+{
+	[TestFixture]
+	public class TParser_Test
+	{
+		[Test]
+		public void TestCase()
+		{
+			var chambers = new TParser().ParseChambers(new TStream("122000010"));
+			foreach(var chamber in chambers)
+			{
+				Console.WriteLine(chamber);
+			}
+		}
+	}
+}
