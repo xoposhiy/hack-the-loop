a5b674886b2afed36bb12a4508c451ff77354b54
xoposhiy@mail.ru <xoposhiy@mail.ru@f78b6cbc-4a89-5bc9-8d0a-bb4248158ca5>
2010-06-20 09:56:16 +0000

    CarsRepo для удобной работы с кучей машинок. Validator - почти написанный валидатор.

---
+++ trunk/CircuitCalc/CarsRepo.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CircuitCalc.TParsing;
+
+namespace CircuitCalc
+{
+	public class CarsRepo
+	{
+		public readonly IDictionary<string, Chamber[]> cars = new Dictionary<string, Chamber[]>();
+		public readonly IDictionary<string, string> encodedCars = new Dictionary<string, string>();
+		public readonly List<string>[] carsByTanksCount = new List<string>[7];
+		private readonly TParser parser = new TParser();
+
+		public CarsRepo(string file)
+		{
+			var lines = File.ReadAllLines(file);
+			for(int i=0; i<lines.Length; i+=2)
+			{
+				var id = lines[i];
+				var encodedCar = lines[i + 1];
+				if(encodedCar == "0") continue;
+				cars.Add(id, parser.ParseCar(encodedCar));
+				encodedCars.Add(id, encodedCar);
+			}
+			for(int tanksCount = 1; tanksCount <= 6; tanksCount++)
+			{
+				carsByTanksCount[tanksCount] = new List<string>();
+				foreach(var car in cars)
+				{
+					var chambers = car.Value;
+					var lastTankIndex = tanksCount - 1;
+					var isSmall = chambers.All(ch => ch.AllTanks().All(i => i <= lastTankIndex) && ch.AllTanks().Any(i => i == lastTankIndex));
+					if(isSmall)
+						carsByTanksCount[tanksCount].Add(car.Key);
+				}
+			}
+		}
+	}
+}
---
+++ trunk/CircuitCalc/FuelValidation/Validator_Test.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+
+namespace CircuitCalc.FuelValidation
+{
+	[TestFixture]
+	public class Validator_Test
+	{
+		Validator v = new Validator();
+		CarsRepo repo = new CarsRepo("../../../Cars.txt");
+
+		[Test]
+		public void ShowCarsByTanksCount()
+		{
+			for(int i = 1; i <= 6; i++)
+				Console.WriteLine(i + " " + repo.carsByTanksCount[i].Count);
+		}
+
+		[Test]
+		public void TestSimpleFuel()
+		{
+			int c = 0;
+			foreach(var carId in repo.carsByTanksCount[1])
+			{
+				var car = repo.cars[carId];
+				try
+				{
+					if(v.FuelFitsCar(car, "11111"))
+					{
+						c++;
+						Console.WriteLine(carId);
+//						car.Print();
+					}
+				}
+				catch
+				{
+					Console.WriteLine(carId);
+					Console.WriteLine(repo.encodedCars[carId]);
+					car.Print();
+					throw;
+				}
+			}
+			Console.WriteLine("TOTAL: " + c);
+		}
+
+	}
+}
