021e2e3b2aa244138b2a2fe0fdd8f24731d1e381
Andrew.Kostousov <Andrew.Kostousov@f78b6cbc-4a89-5bc9-8d0a-bb4248158ca5>
2010-06-20 09:47:16 +0000

    Постилка заводов. Обновлены машинки, они теперь отсортированы по возрастанию идентификаторов
    Авто логин написан, но не работает. Пока на него забью.

--- WebClient/Program.cs
+++ WebClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Linq;
 
 namespace WebClient
 {
@@ -9,8 +10,71 @@ namespace WebClient
 	{
 		static void Main(string[] args)
 		{
-			var c = new WebClient();
-			c.GetCarsList();
+			var sessionId = string.Empty;
+			if (args.Length > 0) sessionId = args[0];
+			var c = new WebClient(sessionId);
+			var error = c.SubmitFuel("2416", fuel);
+			Console.WriteLine(error);
+			GetCars(c);
 		}
+
+		private static void GetCars(WebClient c)
+		{
+			var cars = c.GetCarsList();
+			PrintCars(cars);
+		}
+
+		private static void PrintCars(Dictionary<string, string> cars)
+		{
+			var sb = new StringBuilder();
+			foreach (var car in cars.OrderBy(kvp => int.Parse(kvp.Key)))
+			{
+				sb.AppendLine(car.Key);
+				sb.AppendLine(car.Value);
+			}
+			File.WriteAllText(string.Format("cars-{0}.txt", DateTime.Now.ToString("yyyy-MM-dd-hh-mm")), sb.ToString());
+		}
+
+		private const string success = "Good! The car can use this fuel.";
+		private const string errorAlreadySubmited = "You have already submitted this solution.";
+
+		private const string fuel = @"35L:
+0L2R0#0L1R,
+1R0R0#2R1L,
+3L1L0#35R0R,
+4L3R0#2L3R,
+5L4R0#3L4R,
+7L5R0#4L5R,
+6L7R0#6L7R,
+9L6R0#5L6R,
+8L9R0#8L9R,
+12L8R0#7L8R,
+10L12R0#10L11R,
+11R10R0#12R11L,
+13L11L0#9L10R,
+14L13R0#12L13R,
+16L14R0#13L14R,
+15L16R0#15L16R,
+19L15R0#14L15R,
+17L19R0#17L18R,
+18R17R0#19R18L,
+21L18L0#16L17R,
+20L21R0#20L21R,
+22L20R0#19L20R,
+24L22R0#21L22R,
+23L24R0#23L24R,
+25L23R0#22L23R,
+26L25R0#24L25R,
+28L26R0#25L26R,
+27L28R0#27L28R,
+30L27R0#26L27R,
+29L30R0#29L30R,
+31L29R0#28L29R,
+33L31R0#30L31R,
+32L33R0#32L33R,
+34L32R0#31L32R,
+35R34R0#33L34R,
+X2L0#X34L:
+35L";
 	}
 }
--- WebClient/WebClient.cs
+++ WebClient/WebClient.cs
@@ -1,46 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
-using System.Text.RegularExpressions;
-using NUnit.Framework;
 
 namespace WebClient
 {
 	public class WebClient
 	{
-		private string sessionId = "JSESSIONID=0618F74BE5535FF09182497A418B2B36";
-
-		public void GetCarsList()
+		//TODO: login почему-то не фурычит
+		public WebClient(string sessionId)
 		{
-			var sb = new StringBuilder();
+			this.sessionId = string.IsNullOrEmpty(sessionId) ? Login() : sessionId;
+		}
+
+		public Dictionary<string, string> GetCarsList()
+		{
 			var response = GetResponse(getCarsList);
 			var carIds = HtmlParser.ParseCarsList(response);
+			var cars = new Dictionary<string, string>();
 			foreach (var carId in carIds)
 			{
 				var car = GetCar(carId);
-				sb.AppendLine(carId);
-				sb.AppendLine(car);
+				cars.Add(carId, car);
 			}
-			File.WriteAllText("cars.txt", sb.ToString());
+			return cars;
 		}
 
 		public string GetCar(string carId)
 		{
 			var response = GetResponse(string.Format(getCar, carId));
-			File.WriteAllText("car-response.txt", response);
+			//File.WriteAllText(string.Format("car-{0}.txt", carId), response);
 			var car = HtmlParser.ParseCar(response);
 			return car;
 		}
 
-		private string GetResponse(string requestUri)
+		public string SubmitFuel(string carId, string factory)
 		{
-			var req = WebRequest.Create(requestUri);
-			req.Headers.Add("Cookie", sessionId);
+			var response = Post(string.Format(submitFuel, carId), Escape(factory));
+			File.WriteAllText(string.Format("submit-fuel-for-car-{0}.txt", carId), response);
+			var error = HtmlParser.ParseSubmitFuelResponse(response);
+			return error;
+		}
+
+		private static string Escape(string factory)
+		{
+			return factory.Replace("\r\n", "%0D%0A").Replace("#", "%23").Replace(",", "%2C").Replace(":", "%3A");
+		}
+
+		private string Post(string requestUri, string data)
+		{
+			var req = CreateWebRequest(requestUri);
+			req.Method = "POST";
+			req.ContentType = "application/x-www-form-urlencoded";
+			using (var s = req.GetRequestStream())
+			{
+				var buffer = Encoding.ASCII.GetBytes("contents=" + data);
+				s.Write(buffer, 0, buffer.Length);
+			}
 			using (var resp = req.GetResponse())
 				return GetContent(resp.GetResponseStream());
+			
+		}
+
+		private static string Login()
+		{
+			var req = WebRequest.Create(login);
+			req.Method = "POST";
+			req.ContentType = "application/x-www-form-urlencoded";
+			using (var s = req.GetRequestStream())
+			{
+				var buffer = Encoding.ASCII.GetBytes(string.Format("j_username={0}&j_password={1}", user, pwd));
+				s.Write(buffer, 0, buffer.Length);
+			}
+			using (var resp = req.GetResponse())
+			{
+				var setCookie = resp.Headers["Set-Cookie"];
+				Debug.Assert(setCookie.StartsWith("JSESSIONID="));
+				return setCookie.Split(new[] {';'})[0].Split(new[] {'='})[1];
+			}
+		}
+
+		private string GetResponse(string requestUri)
+		{
+			var req = CreateWebRequest(requestUri);
+			using (var resp = req.GetResponse())
+				return GetContent(resp.GetResponseStream());
+		}
+
+		private WebRequest CreateWebRequest(string requestUri)
+		{
+			var req = WebRequest.Create(requestUri);
+			req.Headers.Add("Cookie", string.Format("JSESSIONID={0}", sessionId));
+			return req;
 		}
 
 		private static string GetContent(Stream stream)
@@ -49,33 +101,14 @@ namespace WebClient
 				return sr.ReadToEnd();
 		}
 
+		private readonly string sessionId;
+		private const string user = @"hack-the-loop";
+		private const string pwd = @"722750249482275797203643486818027156264822953884882105712868";
+		private const string root = @"http://icfpcontest.org/icfp10";
 		private const string login = @"http://icfpcontest.org/icfp10/login";
 		private const string getCarsList = @"http://icfpcontest.org/icfp10/score/instanceTeamCount";
 		private const string submitCar = @"http://icfpcontest.org/icfp10/instance/form";
 		private const string getCar = @"http://icfpcontest.org/icfp10/instance/{0}/solve/form";
 		private const string submitFuel = @"http://icfpcontest.org/icfp10/instance/{0}/solve";
-	}
-
-	static class HtmlParser
-	{
-		public static IEnumerable<string> ParseCarsList(string response)
-		{
-			var matches = CarListItemTemplate.Matches(response);
-			foreach (Match match in matches)
-			{
-				string carId = match.Groups["CarId"].Value;
-				yield return carId;
-			}
-		}
-
-		public static string ParseCar(string response)
-		{
-			var match = CarTemplate.Match(response);
-			string car = match.Groups["Car"].Value;
-			return car;
-		}
-
-		private static readonly Regex CarTemplate = new Regex(@"<label for=.*?>Car:</label>(?<Car>\d*?)</div>");
-		private static readonly Regex CarListItemTemplate = new Regex(@"<tr><td style=.*?>(?<CarId>\d*?)</td><td>(?<NumberOfFuels>\d*?)</td><td>.*?</td></tr>");
 	}
 }
