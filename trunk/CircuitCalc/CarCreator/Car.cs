88d640b608164a48bb9a5993616fad6b0eb0e079
last_g@hackerdom.ru <last_g@hackerdom.ru@f78b6cbc-4a89-5bc9-8d0a-bb4248158ca5>
2010-06-20 13:36:50 +0000

    Проверка связности машинки

---
+++ trunk/CircuitCalc/CarCreator/Car.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CircuitCalc.TParsing;
+
+namespace CircuitCalc.CarCreator
+{
+	/// <summary>
+	///  Класс описывающи отдельную машинку
+	/// Подразумевается что у нас не более 6 баков и они пронумерованны от 0 до 5
+	/// </summary>
+
+	class Car
+	{
+		private readonly Chamber[] chambers;
+		/// <summary>
+		/// Количество баков от 0 до 6
+		/// </summary>
+		private readonly int numOfTanks;
+
+		public Car(Chamber[] chambers, int numOfTanks)
+		{
+			this.chambers = chambers;
+			this.numOfTanks = numOfTanks;
+		}
+
+		public bool IsConnected()
+		{
+			// Создаём пустую матрицу связности
+			var connected = new bool[numOfTanks,numOfTanks];
+			for (var i = 0; i < numOfTanks; ++i )
+			{
+				for (var j = 0; j < numOfTanks; ++j )
+				{
+					connected[i, j] = false;
+				}
+			}
+			// Инициализируем отношение непосредственной связности
+			// Все баки в данном Chamber, которые подключены к верхней, связанны непосредственно
+			// с теми что подключены к нижней
+			foreach (var chamber in chambers)
+			{
+				foreach (var upTrank in chamber.upper)
+				{
+					foreach (var downTrank in chamber.lower)
+					{
+						connected[upTrank, downTrank] = true;
+					}
+				}
+			}
+
+			// Не непосредственная связность. Тут можно сделать в точности 6 шагов.
+
+			for (int i = 0; i < 6; ++i )
+			{
+				for(int tank = 0; tank < numOfTanks; ++tank)
+				{
+					for (int connectedTank = 0; connectedTank < numOfTanks; connectedTank++)
+					{
+						if (connected[tank, connectedTank])
+						{
+							for (int k = 0; k < numOfTanks; k++)
+							{
+								connected[tank, k] |= connected[connectedTank, k];
+							}
+						}
+					}
+				}
+			}
+
+			for (int i = 0; i < numOfTanks; i++)
+			{
+				for (int j = 0; j < numOfTanks; j++)
+				{
+					if (!connected[i, j])
+					{
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+
+	}
+}
---
+++ trunk/CircuitCalc/CarCreator/Car_Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NUnit;
+
+namespace CircuitCalc.CarCreator
+{
+	[TestFixture]
+	class Car_Tests
+	{
+		[Test]
+		public void ConnectednessTest()
+		{
+			var rep = new CarsRepo("../../cars1.txt");
+			for (var i = 0; i < 7; i++)
+			{
+				if (rep.carsByTanksCount[i] == null)
+				{
+					continue;
+				}
+				foreach (var carString in rep.carsByTanksCount[i])
+				{
+					var car = new Car(rep.cars[carString], i);
+					Assert.True(car.IsConnected(), "Машинка не сцепленна...");
+				}
+			}
+		}
+
+	}
+}
